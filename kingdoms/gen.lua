bcd067e05abce6ec18e77959653125b0fe7e62d5
Beha <shacknetisp@mail.com>
2017-01-07 17:58:20 -0500

    Clear old registry before beginning mapgen.

--- kingdoms/gen.lua
+++ kingdoms/gen.lua
@@ -1,8 +1,11 @@
 -- Set flags used by Kingdoms.
 if kingdoms.config.mapgen then
+    minetest.clear_registered_biomes()
+    minetest.clear_registered_decorations()
+
     minetest.set_mapgen_setting("mgname", "v7")
     minetest.set_mapgen_setting("flags", "trees, caves, dungeons, noflat, light, decorations")
-    -- Biomes won't be registered properly until done manually.
+
     default.register_biomes()
     default.register_decorations()
     kingdoms.log("action", "Applied mapgen settings.")

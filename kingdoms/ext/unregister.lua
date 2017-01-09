e40e5342fb9e5f8182d5520a1d1da80f0d817cf8
Beha <shacknetisp@mail.com>
2017-01-08 20:45:26 -0500

    Unregister beds.

--- kingdoms/ext/unregister.lua
+++ kingdoms/ext/unregister.lua
@@ -1,3 +1,7 @@
 -- Things that are not used with Kingdoms' level system.
 minetest.unregister_item("default:key")
 minetest.unregister_item("default:skeleton_key")
+
+-- Kingdoms has it's own spawning system.
+minetest.unregister_item("beds:bed")
+minetest.unregister_item("beds:fancy_bed")

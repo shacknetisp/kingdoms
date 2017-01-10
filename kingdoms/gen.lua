0abe565cb17e40bb272a851c42269cfa3d9662d1
Beha <shacknetisp@mail.com>
2017-01-09 23:09:07 -0500

    Remove unnecessary calls in mapgen setup.

--- kingdoms/gen.lua
+++ kingdoms/gen.lua
@@ -1,14 +1,9 @@
 -- Set flags used by Kingdoms.
 if kingdoms.config.mapgen then
-    minetest.clear_registered_biomes()
-    minetest.clear_registered_decorations()
 
     minetest.set_mapgen_setting("mgname", "v7")
     minetest.set_mapgen_setting("flags", "trees, caves, dungeons, noflat, light, decorations")
 
-    default.register_biomes()
-    default.register_decorations()
-    flowers.register_decorations()
     kingdoms.log("action", "Applied mapgen settings.")
 end
 magic.register_mapgen()
